e89fbbd3d74353739a3d5ee85cf5c7bf637bdab9
Eduardo Marinho <59973014+eduardobivis@users.noreply.github.com>
2021-07-18 14:13:15 -0300

    Delete forkify directory

--- forkify/src/js/config.js
+++
@@ -1,5 +0,0 @@
-//Global Config of the System
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const API_KEY = '3c2e3db3-4968-418e-b16d-b3ba8626e0e7'
